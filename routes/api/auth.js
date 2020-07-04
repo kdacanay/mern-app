9b906c8a38f437929277e868f1aefee2504b7f67
Ken Dacanay <ken.p.r.dacanay@gmail.com>
2020-07-04 03:55:12 -0400

    login successful

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,17 +4,85 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const User = require('../../models/User');
+const { check, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const bcrypt = require('bcryptjs');
+
+
 // @route GET api/auth
 // @desc test route
 // @access Public (no webtoken)
 router.get('/', auth, async (req, res) => {
   try {
-    const user = await (await User.findById(req.user.id)).select('-password');
+    const user = await User.findById(req.user.id).select('-password');
     res.json(user);
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');
   }
 });
+
+// @route POST api/auth
+// @desc  authenticate user and get token
+// @access Public (no webtoken)
+router.post('/',
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists()
+  ],
+  async (req, res) => {
+    // req.body (object of data from request)
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // see if user exists
+    // get users gravatar (based on email)
+    // encrypt password w/bcrypt
+    //return jsonwebtoken (to get logged in right away)
+
+    //destructure req.body
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid credentials' }] });
+      }
+
+      //bcrypt method
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials ' }] });
+      }
+
+      //return jsob webtoken
+      const payload = {
+        user: {
+          id: user.id
+        }
+      }
+
+      jwt.sign(payload,
+        config.get('jwtSecret'),
+        { expiresIn: 3600000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+
+    } catch (err) {
+      console.error(err.message);
+      return res.status(500).send('Server error');
+    }
+  });
 
 module.exports = router;
