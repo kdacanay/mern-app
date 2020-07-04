5da5300264badb063d0c397b2cf938bf6d2151bc
Ken Dacanay <ken.p.r.dacanay@gmail.com>
2020-07-04 02:12:19 -0400

    added something or other

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const gravatar = require('gravatar');
 const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const User = require('../../models/User');
 // @route POST api/users
@@ -56,14 +58,23 @@ router.post('/', [
       await user.save();
 
       //return jsob webtoken
-      res.send('User registered')
+      const payload = {
+        user: {
+          id: user.id
+        }
+      }
+      jwt.sign(payload,
+        config.get('jwtSecret'),
+        { expiresIn: 3600000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       return res.status(500).send('Server error');
     }
-
-
-
   });
 
 module.exports = router;
